2de272febd0500b1aa954f1abfaf3368f14e9eba
karthika-49 <bingikarthika@gmail.com>
2024-02-15 17:29:25 +0530

    added user icon

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,14 @@
 // Navbar.jsx
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 import {
   Flex,
   Button,
+  MenuButton,
+  MenuItem,
+  Menu,
+  MenuList,
+  Avatar,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -10,14 +16,46 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Link as ChakraLink, 
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 import UploadPDF from "./UploadPDF";
+import { backendUrl } from '../constants';
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 
 const Navbar = ({ loggedIn, onLogout }) => {
+  const [user, setUser] = useState({});
   const [uploadModalOpen, setUploadModalOpen] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loggedIn) {
+      fetchUser();
+    }
+  }, [loggedIn]);
+  const fetchUser = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.get(`${backendUrl}user/profile`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      if (!response.data) {
+        // Handle error, e.g., redirect to login
+        navigate("/login");
+        return;
+      }
+      console.log(response.data);
+      setUser(response.data.user);
+      console.log(user.email, user.id);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      // Handle error, e.g., redirect to login
+      navigate("/login");
+    }
+  };
 
   const handleOpenUploadModal = () => {
     setUploadModalOpen(true);
@@ -46,7 +84,7 @@ const Navbar = ({ loggedIn, onLogout }) => {
       <ChakraLink
         as={RouterLink}
         to="/"
-        _hover={{ textDecoration: "none", color: "whitesmoke"}}
+        _hover={{ textDecoration: "none", color: "whitesmoke" }}
       >
         <Flex>Class Library</Flex>
       </ChakraLink>
@@ -56,7 +94,7 @@ const Navbar = ({ loggedIn, onLogout }) => {
           as={RouterLink}
           to="/subjects"
           mx={4}
-          _hover={{ textDecoration: "none", color: "whitesmoke"}}
+          _hover={{ textDecoration: "none", color: "whitesmoke" }}
         >
           Subjects
         </ChakraLink>
@@ -78,7 +116,7 @@ const Navbar = ({ loggedIn, onLogout }) => {
               as={RouterLink}
               to="/signup"
               mx={4}
-              _hover={{ textDecoration: "none", color: "whitesmoke"}}
+              _hover={{ textDecoration: "none", color: "whitesmoke" }}
             >
               Signup
             </ChakraLink>
@@ -92,9 +130,28 @@ const Navbar = ({ loggedIn, onLogout }) => {
             </ChakraLink>
           </>
         ) : (
-          <Button onClick={handleLogout} colorScheme="red" _hover={{ bg: "red.600" }}>
-            Logout
-          </Button>
+          <>
+            <Menu color="black">
+              <MenuButton>
+                <Avatar size="sm" name={user.email} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem style={{ color: "black", fontSize: "15px" }}>
+                  Email : {user.email}
+                </MenuItem>
+                <MenuItem style={{ color: "black", fontSize: "15px" }}>
+                  User Id : {user.userId}
+                </MenuItem>
+              </MenuList>
+            </Menu>
+            <Button
+              onClick={handleLogout}
+              colorScheme="red"
+              _hover={{ bg: "red.600" }}
+            >
+              Logout
+            </Button>
+          </>
         )}
       </Flex>
 
